153aa6c5b012bd7cb53b05ddc523333d3bf74419
lessly <treessleess@gmail.com>
2019-11-27 18:48:13 -0600

    Updates

---
+++ precache-manifest.98390406a79b0ea00d5d6b0298561910.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95d7c05bb457ba00df0d098594f55ec6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "429700840852e9c0d5f4",
+    "url": "/portfolio/static/css/main.c8186f9a.chunk.css"
+  },
+  {
+    "revision": "c0487721605d7d73dd07",
+    "url": "/portfolio/static/js/2.ef4e95ae.chunk.js"
+  },
+  {
+    "revision": "429700840852e9c0d5f4",
+    "url": "/portfolio/static/js/main.d7a63bc7.chunk.js"
+  },
+  {
+    "revision": "a46b3fd8f34f8691f45e",
+    "url": "/portfolio/static/js/runtime-main.6c1b9ebb.js"
+  },
+  {
+    "revision": "7cc4069a96ee6aebfbd24b67ae6979a8",
+    "url": "/portfolio/static/media/Me.7cc4069a.jpg"
+  },
+  {
+    "revision": "432ea07580a3523a61ee007deb59370d",
+    "url": "/portfolio/static/media/Palmeras.432ea075.jpg"
+  }
+]);
